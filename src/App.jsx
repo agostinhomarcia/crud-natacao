c539aa9bc1679150b20508c7203430910a6e9bf0
Márcia Agostinho <106019020+agostinhomarcia@users.noreply.github.com>
2023-08-07 20:08:02 -0300

    alterando estilos

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import { SiReacthookform } from "react-icons/si";
 import { Outlet } from "react-router-dom";
 import NavLinks from "./components/Nav/NavLinks";
 import "./app.css";
+
+import Logo from "../public/nadando.svg";
+
 function App() {
   return (
     <div className="container">
@@ -9,9 +12,13 @@ function App() {
         <div className="header-title">
           <SiReacthookform className="icon" />
           <h1 className="title">Cadastro de alunos para aulas de natação</h1>
+          <div className="container-img">
+            <img src={Logo} alt="Logo" className="image-logo" />
+          </div>
         </div>
         <NavLinks />
       </header>
+
       <Outlet />
     </div>
   );
